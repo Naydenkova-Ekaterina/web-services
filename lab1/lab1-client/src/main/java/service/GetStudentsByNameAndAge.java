75751a7430be30d006dcbd6964cbab8f667ea46f
Naydenkova-Ekaterina <38037212+Naydenkova-Ekaterina@users.noreply.github.com>
2021-03-20 02:25:41 +0300

    Web Services - Lab 1 - client

---
+++ lab1/lab1-client/src/main/java/service/GetStudentsByNameAndAge.java
@@ -0,0 +1,79 @@
+
+package service;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for getStudentsByNameAndAge complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType name="getStudentsByNameAndAge">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="studentName" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *         &lt;element name="studentAge" type="{http://www.w3.org/2001/XMLSchema}int"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "getStudentsByNameAndAge", propOrder = {
+    "studentName",
+    "studentAge"
+})
+public class GetStudentsByNameAndAge {
+
+    protected String studentName;
+    protected int studentAge;
+
+    /**
+     * Gets the value of the studentName property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getStudentName() {
+        return studentName;
+    }
+
+    /**
+     * Sets the value of the studentName property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setStudentName(String value) {
+        this.studentName = value;
+    }
+
+    /**
+     * Gets the value of the studentAge property.
+     * 
+     */
+    public int getStudentAge() {
+        return studentAge;
+    }
+
+    /**
+     * Sets the value of the studentAge property.
+     * 
+     */
+    public void setStudentAge(int value) {
+        this.studentAge = value;
+    }
+
+}
---
+++ lab1/lab1-client/src/main/java/service/GetStudentsBySurnameAndAgeAndFaculty.java
@@ -0,0 +1,106 @@
+
+package service;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for getStudentsBySurnameAndAgeAndFaculty complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType name="getStudentsBySurnameAndAgeAndFaculty">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="studentSurname" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *         &lt;element name="studentAge" type="{http://www.w3.org/2001/XMLSchema}int"/>
+ *         &lt;element name="studentFaculty" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "getStudentsBySurnameAndAgeAndFaculty", propOrder = {
+    "studentSurname",
+    "studentAge",
+    "studentFaculty"
+})
+public class GetStudentsBySurnameAndAgeAndFaculty {
+
+    protected String studentSurname;
+    protected int studentAge;
+    protected String studentFaculty;
+
+    /**
+     * Gets the value of the studentSurname property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getStudentSurname() {
+        return studentSurname;
+    }
+
+    /**
+     * Sets the value of the studentSurname property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setStudentSurname(String value) {
+        this.studentSurname = value;
+    }
+
+    /**
+     * Gets the value of the studentAge property.
+     * 
+     */
+    public int getStudentAge() {
+        return studentAge;
+    }
+
+    /**
+     * Sets the value of the studentAge property.
+     * 
+     */
+    public void setStudentAge(int value) {
+        this.studentAge = value;
+    }
+
+    /**
+     * Gets the value of the studentFaculty property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getStudentFaculty() {
+        return studentFaculty;
+    }
+
+    /**
+     * Sets the value of the studentFaculty property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setStudentFaculty(String value) {
+        this.studentFaculty = value;
+    }
+
+}
